b510f1bcec4cb7e537110a1d815dc7a764f93026
Thomas Junk <thomas.junk@intevation.de>
2022-03-02 08:47:03 +0100

    org roam

--- init.el
+++ init.el
@@ -326,7 +326,7 @@
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory (file-truename "/path/to/org-files/"))
+  (org-roam-directory (file-truename "~/Dokumente/arbeit/zettelkasten"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
