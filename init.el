62eabbf340055e5027cb30b4392e3e0bfcf4ded0
Thomas Junk <thomas.junk@intevation.de>
2022-03-03 13:01:27 +0100

    Custom keybindings for org clock

--- init.el
+++ init.el
@@ -273,6 +273,8 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cI" 'org-clock-in)
+(global-set-key "\C-cO" 'org-clock-out)
 (setq org-log-done 'time)
 
 (add-hook 'org-mode-hook 'org-appear-mode)
@@ -574,7 +576,7 @@
       (setq helm-google-suggest-use-curl-p t))
 
 (setq helm-split-window-in-side-p t ; open helm buffer inside current window, not occupy whole other window
-  helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
+  helm-move-to-line-cycle-in-source t ; move to end or beginning of source wihen reaching top or bottom of source.
   helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
   helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
   helm-ff-file-name-history-use-recentf t
