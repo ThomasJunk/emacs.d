a0de0ecebc7ff6e0f50974cff5ddb058da99da04
Thomas Junk <thomas.junk@intevation.de>
2022-03-12 16:51:01 +0100

    remove org roam

--- init.el
+++ init.el
@@ -376,25 +376,6 @@
 ;; special extensions for markdown_github output
 (setq org-pandoc-format-extensions '(markdown_github+pipe_tables+raw_html))
 
-;;org roam
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory (file-truename "~/Dokumente/arbeit/zettelkasten"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
-  :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
-
 
 ;; org-download
 (require 'org-download)
